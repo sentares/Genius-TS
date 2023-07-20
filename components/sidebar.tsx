648bde43101ecb43ca1e138c3da38810415b3e07
Aidar Arzybekov Tashtanbekovich <aidararzybekovtashtanbekovich@MacBook-Air-Aidar.local>
2023-07-20 13:44:13 +0600

    created dashoard page

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,6 +3,7 @@
 import { Montserrat } from 'next/font/google'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 import { cn } from '@/lib/utils'
 import {
@@ -62,6 +63,7 @@ const routes = [
 ]
 
 const Sidebar = () => {
+	const pathname = usePathname()
 	return (
 		<div className='space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white'>
 			<div className='px-3 py-3 flex-1'>
@@ -79,10 +81,10 @@ const Sidebar = () => {
 							key={route.href}
 							href={route.href}
 							className={cn(
-								'text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition'
-								// pathname === route.href
-								// 	? 'text-white bg-white/10'
-								// 	: 'text-zinc-400'
+								'text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition',
+								pathname === route.href
+									? 'text-white bg-white/10'
+									: 'text-zinc-400'
 							)}
 						>
 							<div className='flex items-center flex-1'>
